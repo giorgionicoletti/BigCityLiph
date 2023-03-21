c58d76f8ddaae23c4f7086d4601067e5fabc94d8
Giorgio Nicoletti <gnicoletti.mail@gmail.com>
2023-03-21 18:18:32 +0100

    Win the hunger games

---
+++ nuclearsilos/thermonucleardevice.py
@@ -0,0 +1,11 @@
+# very useful to win the hunger games
+from physics import nuclear
+
+bomb_core = nuclear.fission()
+bomb_th = nuclear.fusion()
+
+import engeneering
+
+bomb = engeneering.combine(bomb_core, bomb_th)
+
+bomb.boom()
